42b82b541a8bf3244fdf6d1d8ef5005f914ac190
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-10-14 00:04:05 +0200

    Additional properties can be added to the factory.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,28 +1,31 @@
 import { assertThat, hasProperties, not, hasProperty } from 'hamjest';
 
-const factory = (definition) => () => definition;
-const trait = (definition) => {
-  const fn = () => definition;
-  fn.__isTrait = true;
-  return fn;
-};
-
-const create = (factoryFn, ...args) => {
-  const definition = factoryFn();
+const factory = (definition) => (...args) => {
   const definitionWithoutTraits = Object.keys(definition).reduce((result, key) => {
-    if (typeof definition[key] === 'function' && definition[key].__isTrait) { return result; }
+    if (isTrait(definition[key])) { return result; }
     result[key] = definition[key];
     return result;
   }, {});
 
   return args.reduce((result, arg) => Object.assign(
     result,
-    definition[arg](),
+    isFunction(definition[arg]) ? definition[arg]() : arg,
   ), definitionWithoutTraits);
 };
 
+const isFunction = (probablyFunction) =>
+  typeof probablyFunction === 'function';
 
-const userFactory = factory({
+const isTrait = (probablyTrait) =>
+  isFunction(probablyTrait) && probablyTrait.__isTrait;
+
+const trait = (definition) => {
+  const fn = () => definition;
+  fn.__isTrait = true;
+  return fn;
+};
+
+const createUser = factory({
   id: 1,
   name: 'Peter',
   isDisabled: trait({
@@ -32,16 +35,21 @@ const userFactory = factory({
 
 describe('create', () => {
   it('responds correct attributes', () => {
-    assertThat(create(userFactory), hasProperties({ id: 1, name: 'Peter' }));
+    assertThat(createUser(), hasProperties({ id: 1, name: 'Peter' }));
   });
 
   it('doesn\'t include traits', () => {
-    assertThat(create(userFactory), not(hasProperty('isDisabled')));
+    assertThat(createUser(), not(hasProperty('isDisabled')));
   });
 
-  it('trait works as well', () => {
-    assertThat(create(userFactory, 'isDisabled'),
+  it('trait are added correctly', () => {
+    assertThat(createUser('isDisabled'),
       hasProperties({ id: 1, name: 'Peter', disabledAt: '2000-01-01' }));
+  });
+
+  it('additional properties can be added', () => {
+    assertThat(createUser({ additional: 'property' }),
+      hasProperties({ additional: 'property' }));
   });
 });
 
