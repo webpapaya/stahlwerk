02e6db05a4863499cc0ee32de611c5d27f7565cd
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-10-13 23:47:20 +0200

    Added traits.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,8 +1,47 @@
-import { assertThat, equalTo } from 'hamjest';
+import { assertThat, hasProperties, not, hasProperty } from 'hamjest';
 
-describe('hallo', () => {
-  it('', () => {
+const factory = (definition) => () => definition;
+const trait = (definition) => {
+  const fn = () => definition;
+  fn.__isTrait = true;
+  return fn;
+};
 
+const create = (factoryFn, ...args) => {
+  const definition = factoryFn();
+  const definitionWithoutTraits = Object.keys(definition).reduce((result, key) => {
+    if (typeof definition[key] === 'function' && definition[key].__isTrait) { return result; }
+    result[key] = definition[key];
+    return result;
+  }, {});
+
+  return args.reduce((result, arg) => Object.assign(
+    result,
+    definition[arg](),
+  ), definitionWithoutTraits);
+};
+
+
+const userFactory = factory({
+  id: 1,
+  name: 'Peter',
+  isDisabled: trait({
+    disabledAt: '2000-01-01',
+  }),
+});
+
+describe('create', () => {
+  it('responds correct attributes', () => {
+    assertThat(create(userFactory), hasProperties({ id: 1, name: 'Peter' }));
+  });
+
+  it('doesn\'t include traits', () => {
+    assertThat(create(userFactory), not(hasProperty('isDisabled')));
+  });
+
+  it('trait works as well', () => {
+    assertThat(create(userFactory, 'isDisabled'),
+      hasProperties({ id: 1, name: 'Peter', disabledAt: '2000-01-01' }));
   });
 });
 
